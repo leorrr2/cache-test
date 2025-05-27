cdbdcda661a79eab1d9d14e03a89b3dd9b6bab28
Leo Reuter <leo@leo-reuter.de>
2025-05-27 13:36:32 +0200

    cache test

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const time = await getTime();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center justify-items-center min-h-screen p-8 pb-20">
       <h1>Hello World - {time}</h1>
     </div>
   );
