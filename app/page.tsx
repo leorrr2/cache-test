7e068873754838366eb1b089313d88ac1ad174e4
Leo Reuter <leo@leo-reuter.de>
2025-05-27 13:04:34 +0200

    cache test

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,18 @@ const url =
   process.env.NODE_ENV === "production"
     ? `https://${process.env.VERCEL_BRANCH_URL}`
     : "http://localhost:3000";
+
 const getTime = async () => {
-  const response = await fetch(`${url}/api/time`, {
-    next: { tags: ["time"] },
-  });
-  const data = await response.json();
-  return data.time;
+  try {
+    const response = await fetch(`${url}/api/time`, {
+      next: { tags: ["time"] },
+    });
+    const data = await response.json();
+    return data.time;
+  } catch (error) {
+    console.error(error);
+    return "fallback";
+  }
 };
 
 export default async function Home() {
